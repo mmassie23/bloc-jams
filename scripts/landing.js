c531bbd7c948e33ed158b67992d386f9ef925a98
Marquis Massie <marquismassie23@Marquiss-MacBook-Pro.local>
2016-09-17 23:29:23 -0700

    Adding Event Listeners

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,10 +1,24 @@
-var animatePoints = function () {
-    var revealPoint = function (index){
-        for(var i = 0; i <= index; i++){
-            points[i].style.opacity = 1;
-            points[i].style.transform = "scaleX(1) translateY(0)";
-            points[i].style.msTransform = "scaleX(1) translateY(0)";
-            points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-        }
-    }
+var pointsArray = document.getElementByClassName('point');
+var animatePoints = function(points){
+    forEach(points, revealPoint);
+    var revealPoint = function(index){
+        points[index].style.opacity = 1;
+        points[index].style.transform = "scaleX(1) translateY(0)";
+        points[index].style.msTransform = "scaleX(1) translateY(0)";
+        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+    
+    };
 };
+window.onload = function(){
+    if(window.innerHeight > 950){
+        animatePoints(pointsArray);
+    }
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    window.addEventListener('scroll', function(event){
+       //console.log("Current offset from the top is "+sellingPoints.getBoundingClientRect().top+" pixels"); 
+       if(document.ELEMENT.scrollTop || document.body.scrollTop >= scrollDistance){
+           animatePoints(pointsArray);
+       }
+    });
+}
---
+++ scripts/utilities.js
@@ -0,0 +1,5 @@
+function forEach(arr, funct){
+    for(var i = 0; i < arr.length; i++){
+        funct(arr[i]);
+    }
+}
