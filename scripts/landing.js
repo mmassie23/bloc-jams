49f71c4e2e243c894a12cc0c4d16a362c94edf62
Marquis Massie <marquismassie23@Marquiss-MacBook-Pro.local>
2016-09-29 08:26:33 -0700

    Refractoring album JavaScript

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,24 +1,22 @@
-var pointsArray = document.getElementByClassName('point');
-var animatePoints = function(points){
-    forEach(points, revealPoint);
-    var revealPoint = function(index){
-        points[index].style.opacity = 1;
-        points[index].style.transform = "scaleX(1) translateY(0)";
-        points[index].style.msTransform = "scaleX(1) translateY(0)";
-        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-    
+var animatePoints = function(){
+    var revealPoint = function(){
+       $(this).css({
+           opacity: 1,
+           transform: 'scale(1) translateY(0)'
+       });
     };
+    $.each($('.point'), revealPoint);
 };
-window.onload = function(){
-    if(window.innerHeight > 950){
-        animatePoints(pointsArray);
+$(window).load(function(){
+    if ($(window).height() > 950){
+        animatePoints();
     }
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-    window.addEventListener('scroll', function(event){
+    var scrollDistance = $('.selliing-points').offset().top - $(window).height() + 200;
+    
+    $(window).scroll("scroll", function(event){
        //console.log("Current offset from the top is "+sellingPoints.getBoundingClientRect().top+" pixels"); 
-       if(document.ELEMENT.scrollTop || document.body.scrollTop >= scrollDistance){
-           animatePoints(pointsArray);
+       if($(window).scrollTop() >= scrollDistance){
+           animatePoints();
        }
     });
-}
+});
